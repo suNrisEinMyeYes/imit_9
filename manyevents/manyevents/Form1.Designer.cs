ca66b4e1055458948b31e8a79cf34509c3e7ed21
suNrisEinMyeYes <iklown3@gmail.com>
2020-05-13 22:55:09 +0700

    fix

--- manyevents/manyevents/Form1.Designer.cs
+++ manyevents/manyevents/Form1.Designer.cs
@@ -193,6 +193,11 @@
             // numericUpDown1
             // 
             this.numericUpDown1.Location = new System.Drawing.Point(227, 372);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
             this.numericUpDown1.Name = "numericUpDown1";
             this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
             this.numericUpDown1.TabIndex = 25;
